274bc128ab8570adee176062a4083836c8b6681e
Swetlana <sweta_zaika@mail.ru>
2022-09-12 23:22:59 +0300

    частично получатся стало задание

--- src/Main.java
+++ src/Main.java
@@ -1,87 +1,94 @@
+import java.util.Arrays;
+
 public class Main {
 
-    private static final Employee[] allEmployee = new Employee[10];
+    // private static final Employee[] allEmployee = new Employee[10];
 
     public static void main(String[] args) {
+        Employee[] employeeParameters = new Employee[10];
+        Employee empl1 = new Employee("Петров ", "Петр", "ПетрПетрович", 4, 300_000);
+        Employee empl2 = new Employee("Иванов ", "Иван", "ПетрПетрович", 1, 200_000);
+        Employee empl3 = new Employee("Чудаков ", "Дмитрий", "Сергеевич", 3, 400_000);
+        Employee empl4 = new Employee("Кулик ", "Татьяна", "Ивановна", 3, 120_000);
+        Employee empl5 = new Employee("Лазаренко ", "Игорь", "Геннадьевич", 1, 220_000);
+        Employee empl6 = new Employee("Титкова ", "Галина", "Ивановна", 5, 55_000);
+        Employee empl7 = new Employee("Петриченко ", "Любовь", "Васильевна", 1, 500_000);
+        Employee empl8 = new Employee("Шведова ", "Елена", "Евгеньевна", 2, 450_000);
+        Employee empl9 = new Employee("Песенко ", "Валерий", "Николаевич", 1, 440_000);
+        Employee empl10 = new Employee("Блохина ", "Юлия", "Алексеевна", 2, 142_000);
 
-        Employee firstEmployee = new Employee("Петров ", "Петр", "ПетрПетрович", 4, 300_000);
-        Employee secondtEmployee = new Employee("Иванов ", "Иван", "ПетрПетрович", 1, 200_000);
-        Employee thirdEmployee = new Employee("Чудаков ", "Дмитрий", "Сергеевич", 3, 400_000);
-        Employee fourthEmployee = new Employee("Кулик ", "Татьяна", "Ивановна", 3, 120_000);
-        Employee fifthEmployee = new Employee("Лазаренко ", "Игорь", "Геннадьевич", 1, 220_000);
-        Employee sixthEmployee = new Employee("Титкова ", "Галина", "Ивановна", 5, 55_000);
-        Employee seventhEmployee = new Employee("Петриченко ", "Любовь", "Васильевна", 1, 500_000);
-        Employee eighthEmployee = new Employee("Шведова ", "Елена", "Евгеньевна", 2, 450_000);
-        Employee ninthEmployee = new Employee("Песенко ", "Валерий", "Николаевич", 1, 440_000);
-        Employee tenthEmployee = new Employee("Блохина ", "Юлия", "Алексеевна", 2, 142_000);
+        employeeParameters[0] = empl1;
+        employeeParameters[1] = empl2;
+        employeeParameters[2] = empl3;
+        employeeParameters[3] = empl4;
+        employeeParameters[4] = empl5;
+        employeeParameters[5] = empl6;
+        employeeParameters[6] = empl7;
+        employeeParameters[7] = empl8;
+        employeeParameters[8] = empl9;
+        employeeParameters[9] = empl10;
 
-        allEmployee[0] = firstEmployee;
-        allEmployee[1] = secondtEmployee;
-        allEmployee[2] = thirdEmployee;
-        allEmployee[3] = fourthEmployee;
-        allEmployee[4] = fifthEmployee;
-        allEmployee[5] = sixthEmployee;
-        allEmployee[6] = seventhEmployee;
-        allEmployee[7] = eighthEmployee;
-        allEmployee[8] = ninthEmployee;
-        allEmployee[9] = tenthEmployee;
+
         //  1. Получить список всех сотрудников со всеми имеющимися по ним данными (вывести в консоль значения всех полей (toString)).
-        for (int i = 0; i < allEmployee.length; i++) {
-            System.out.println(allEmployee[i]);
-        }
-    }
+        // for (Employee employee : employeeParameters) ;
 
-    //    2. Посчитать сумму затрат на зарплаты в месяц.
-    public static void calculateMonthWages() {
+        for (int i = 0; i < employeeParameters.length; i++) {
+            System.out.println(employeeParameters[i]);
+        }
+        //    2. Посчитать сумму затрат на зарплаты в месяц.
         int sum = 0;
-        for (Employee employee : allEmployee) {
-            sum += employee.getWages();
+        for (int i = 0; i < employeeParameters.length; i++) {
+            sum += employeeParameters[i].getWages();
         }
-
-        System.out.println("Сумма заработной платы " + sum + " рублей");
-
-    }
-
-    //    3. Найти сотрудника с максимальной зарплатой.
-    public static void toFindMaxWages() {
-        int maxWages = -1;
-        for (Employee employee : allEmployee) {
-            if (employee != null) {
-                if (employee.getWages() > maxWages) {
-                    maxWages = employee.getWages();
-                }
-            }
-        }
-        System.out.println("максимальная запрплата " + maxWages + ".");
-    }
-
-    //    4. Найти сотрудника с минимальной зарплатой.
-    public static void findMinWages() {
-
-        int minWages = allEmployee[0].getWages();
-        for (Employee employee : allEmployee) {
-            if (employee != null) {
-                if (employee.getWages() < minWages) {
-                    minWages = employee.getWages();
-                }
-            }
-        }
-        System.out.println("минимальная зарплата " + minWages);
-    }
+        System.out.println("Сумма затрат на зарплаты в меяц составила: " + sum + " руб.");
 
 
-    // 5. Подсчитать среднее значение зарплат (можно использовать для этого метод из пункта b).
-    //   public static double averageWage () {
-    //      return totalWages() / (double) allEmployee.length;
-    //   }
-
-    // 6. Получить Ф. И. О. всех сотрудников (вывести в консоль).
-    public static void printFullName() {
-        for (Employee employee : allEmployee) {
-            {
-                System.out.println(employee.toString());
-            }
-
-        }
     }
 }
+
+//    3. Найти сотрудника с максимальной зарплатой.
+//  int max = 0;
+//for (Employee employee:employeeParameters) {
+//  if (employeeParameters[i].getWages()>max);{
+//max=employeeParameters[i].getWages();
+
+
+//System.out.println("Максимальная зарплата" + max);
+
+
+// int max = day[0];
+//        for (int dayMax : day) {
+//            if (dayMax > max) {
+//                max = dayMax;
+
+
+//    4. Найти сотрудника с минимальной зарплатой.
+//public static void findMinWages() {
+
+//  int minWages = allEmployee[0].getWages();
+//for (Employee employee : allEmployee) {
+//  if (employee != null) {
+//    if (employee.getWages() < minWages) {
+//      minWages = employee.getWages();
+//}
+//}
+//}
+//System.out.println("минимальная зарплата " + minWages);
+//}
+
+
+// 5. Подсчитать среднее значение зарплат (можно использовать для этого метод из пункта b).
+//   public static double averageWage () {
+//      return totalWages() / (double) allEmployee.length;
+//   }
+
+// 6. Получить Ф. И. О. всех сотрудников (вывести в консоль).
+//public static void printFullName() {
+//  for (Employee employee : allEmployee) {
+//    {
+//      System.out.println(employee.toString());
+// }
+
+//}
+//}
+//}
